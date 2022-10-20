276a85fe930e663dd467fe7d5fe4d623fc28e8ff
skynettx <apsmu@protonmail.com>
2022-10-21 00:17:35 +0200

    Refactored objects.cpp and objects.h

--- src/objects.h
+++ src/objects.h
@@ -43,56 +43,56 @@ enum OBJ_TYPE
 #define FIRST_SPECIAL ( S_DUMB_MISSLE )
 
 struct objlib_t {
-    int item;
-    int numframes;
-    int payAmount;
-    int resaleAmount;
-    int f_10;
-    int (*f_14)(int);
-    int f_18;
-    int f_1c;
-    int f_20;
-    int f_24;
-    int f_28;
-    int moneyflag;
-    int f_30;
-    int f_34;
+    int item;                             // LUMPNUM OF PIC
+    int numframes;                        // NUMBER OF FRAMES
+    int cost;                             // COST OF OBJECT
+    int start_cnt;                        // START AMOUNT OF ITEMS
+    int max_cnt;                          // MAX COUNT OF UNITS/ITEMS
+    int (*actf)(int);                     // FUNCTION PTR FOR ACTION
+    int fxtype;                           // SOUND FX TYPE
+    int forever;                          // LAST FOREVER
+    int onlyflag;                         // UNIQUE FLAG ( ENERGY )
+    int loseit;                           // LOSE OBJECT
+    int specialw;                         // SPECIAL WEAPON
+    int moneyflag;                        // ADDS MONEY TO PLAYER WHEN PICKED UP
+    int game1flag;                        // TRUE=AVALABLE IN ALL GAMES
+    int shieldflag;                       // SHOW SHIELD WHEN BONUS
 };
 
 struct object_t {
-    object_t* f_0;
+    object_t* prev;                       // LINK LIST PREV
     object_t* next;                       // LINK LIST NEXT
-    int f_8;
-    int f_c;
-    objlib_t *f_10;
-    int f_14;
+    int num;                              // NUMBER OF ITEM(S)
+    int type;                             // OBJECT TYPE
+    objlib_t *lib;                        // POINTER TO LIB
+    int inuse;                            // CURRENTLY EQUIPED
 };
 
 extern object_t first_objs, last_objs;
 
 void OBJS_Init(void);
-int OBJS_Add(int a1);
+int OBJS_Add(int type);
 void OBJS_GetNext(void);
-int OBJS_GetAmt(int a1);
-objlib_t *OBJS_GetLib(int a1);
-int OBJS_CanBuy(int a1);
-int OBJS_GetCost(int a1);
-int OBJS_CanSell(int a1);
+int OBJS_GetAmt(int type);
+objlib_t *OBJS_GetLib(int type);
+int OBJS_CanBuy(int type);
+int OBJS_GetCost(int type);
+int OBJS_CanSell(int type);
 int OBJS_GetNum(void);
-int OBJS_IsOnly(int a1);
-int OBJS_GetTotal(int a1);
-int OBJS_GetResale(int a1);
-int OBJS_Buy(unsigned int a1);
-int OBJS_Sell(int a1);
+int OBJS_IsOnly(int type);
+int OBJS_GetTotal(int type);
+int OBJS_GetResale(int type);
+int OBJS_Buy(unsigned int type);
+int OBJS_Sell(int type);
 void OBJS_Clear(void);
-int OBJS_Load(object_t *a1);
-int OBJS_IsEquip(int a1);
+int OBJS_Load(object_t *inobj);
+int OBJS_IsEquip(int type);
 int OBJS_LoseObj(void);
 void OBJS_DisplayStats(void);
-int OBJS_MakeSpecial(int a1);
-void OBJS_Use(int a1);
+int OBJS_MakeSpecial(int type);
+void OBJS_Use(int type);
 void OBJS_Think(void);
-int OBJS_SubEnergy(int a1);
-int OBJS_AddEnergy(int a1);
+int OBJS_SubEnergy(int amt);
+int OBJS_AddEnergy(int amt);
 void OBJS_CachePics(void);
 void OBJS_FreePics(void);
