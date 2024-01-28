f2b167efbf906f8eb1ea82bbfffcb6f2afc1cb30
skynettx <apsmu@protonmail.com>
2024-01-28 20:50:12 +0100

    Refactored prefapi.cpp and prefapi.h

--- src/prefapi.h
+++ src/prefapi.h
@@ -1,11 +1,11 @@
 #pragma once
-int INI_InitPreference(const char *section);
-int INI_GetPreferenceLong(const char *section, const char *key, int defValue);
-int INI_GetPreferenceHex(const char* section, const char* key, int defValue);
-int INI_GetPreferenceBool(const char *section, const char *key, short defvalue);
-char* INI_GetPreference(const char *section, const char *key, char *retValue, int lenght, const char *defValue);
-int INI_PutPreferenceLong(const char *section, const char *key, int value);
-int INI_PutPreferenceHex(const char *section, const char *key, int value);
-int INI_PutPreferenceBool(const char *section, const char *key, short value);
-int INI_PutPreference(const char *section, const char *key, const char *value);
-int INI_DeletePreference(const char *section, const char *key);
+int INI_InitPreference(const char *profile);
+int INI_GetPreferenceLong(const char *section, const char *option, int def);
+int INI_GetPreferenceHex(const char *section, const char *option, int def);
+int INI_GetPreferenceBool(const char *section, const char *option, short def);
+char* INI_GetPreference(const char *section, const char *option, char *buf, int buflen, const char *def);
+int INI_PutPreferenceLong(const char *section, const char *option, int val);
+int INI_PutPreferenceHex(const char *section, const char *option, int val);
+int INI_PutPreferenceBool(const char *section, const char *option, short val);
+int INI_PutPreference(const char *section, const char *option, const char *val);
+int INI_DeletePreference(const char *section, const char *option);
